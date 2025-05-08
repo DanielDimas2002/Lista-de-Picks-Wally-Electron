e30a12e649fa0be25f58f087882194a806fa218b
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-05-08 10:32:07 -0300

    Adição de CSS

---
+++ app-react/src/componentes/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Menu(){
+    <header className={estilos.header}>
+        <nav className={estilos.nav}>
+            <h1>Gestão de Campeões</h1>
+            <button className={estilos.botao}>Navegar</button>
+        </nav>
+    </header>
+}
+export default Menu
