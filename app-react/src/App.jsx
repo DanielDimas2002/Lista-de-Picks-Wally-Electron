8ecd1e085f9c1c06838d88051344931d6a78d555
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-05-09 10:29:50 -0300

    Criação do Aside

--- app-react/src/App.jsx
+++ app-react/src/App.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Menu from './componentes/Menu/Menu';
+import Aside from "./componentes/Aside/Aside"
 import TabelaVida from "./componentes/TabelaVida/TabelaVida";
 import TabelaPick from "./componentes/TabelaPick/TabelaPick";
 import TabelaBanco from "./componentes/TabelaBanco/TabelaBanco";
---
+++ app-react/src/componentes/Aside/Aside.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Aside.module.css";
+
+function Aside({handleCadastrarPicks, handleCadastrarVida, handleCadastrarBanco}){
+
+    return(
+        <aside className="aside-container">
+            <h2>Cadastro</h2>
+            <h3></h3>
+        </aside>
+    )
+
+}
+export default Aside
