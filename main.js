a53e9698468f5698b351036da04c3f4c04c77b5d
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2026-05-02 11:55:37 -0300

    comentando o Devtools automático

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function createWindow() {
   });
 
   mainWindow.loadFile(path.join(__dirname, 'app-react', 'build', 'index.html'));
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   mainWindow.on('close', function () {
     mainWindow = null;
